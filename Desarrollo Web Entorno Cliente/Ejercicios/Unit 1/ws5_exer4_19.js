8da8291c017b9b67d99e6234d4cc6fe77d268a46
unknown <egallegoc98@gmail.com>
2019-11-02 16:08:45 +0100

    20191102

--- Desarrollo Web Entorno Cliente/Ejercicios/Unit 1/ws5_exer4_19.js
+++ Desarrollo Web Entorno Cliente/Ejercicios/Unit 1/ws5_exer4_19.js
@@ -2,11 +2,11 @@ var width = parseInt(prompt("Enter column width in pixels:"));
 
 document.write("<table>");
 for(let i = 1; i <= 8; i++) {
-    document.write("<tr height=" + width + ">");
+    document.write("<tr width=" + width + ">");
     for(let j = 1; j <= 8; j ++) {
         if(i % 2 != 0)
             if(j % 2 != 0)
-                document.write("<td id=\"black\" width=" + width + "></td>");
+                document.write("<td id= '{black}' width=" + width + "></td>");
             else
                 document.write("<td width=" + width + "></td>");
         else
